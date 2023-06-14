3bc619075d323733094aafeb7bad0ed42979a19c
JPabloGozalezT <54081160+juanpablogt@users.noreply.github.com>
2023-06-14 15:33:46 +0000

    l,lcl,xlc,x

--- presentacion/python.py
+++ presentacion/python.py
@@ -11,3 +11,9 @@ print(" Hola Mundo")
 print(" Hola Mundo")
 print(" Hola Mundo")
 print(" Hola Mundo")
+
+# Path: presentacion/python.py
+print(" Hola Mundo")
+print(" Hola Mundo")
+print(" Hola Mundo")
+
