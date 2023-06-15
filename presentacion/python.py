7917469d4d666850631f4b5ca7a393eb79b152a6
JPabloGozalezT <54081160+juanpablogt@users.noreply.github.com>
2023-06-15 15:37:15 +0000

    lentandself

--- presentacion/python.py
+++ presentacion/python.py
@@ -1,19 +1,15 @@
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
+class Students:
+    def __init__(self, names):
+        self.names = names
+    
+    def count(self):
+        return len(self.names)
+    
+    def __str__(self):
+        return ', '.join(self.names)
 
-# Path: presentacion/python.py
-print(" Hola Mundo")
-print(" Hola Mundo")
-print(" Hola Mundo")
+names = ['Alice', 'Bob', 'Charlie']
+students = Students(names)
 
+print(students.count())  # Output: 3
+print(students)  
