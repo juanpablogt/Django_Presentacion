1dc0b734ac760848bcc66e349e4501c08bf932b5
JPabloGozalezT <54081160+juanpablogt@users.noreply.github.com>
2023-06-15 18:42:29 +0000

    Django

--- presentacion/listas.py
+++ presentacion/listas.py
@@ -1,10 +1,6 @@
-my_list1 = [1, 2, 3, 4, 5]
-my_list2 = []
 
-for value in my_list1:
-    result = value + 42
-    my_list2.append(result)
-    print(result)
+my_list = list(range(1001))
+print(my_list)
 
 
 
