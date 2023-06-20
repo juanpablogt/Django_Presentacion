67a820e5db4740d5bed473212ff0599e67dca2cb
JPabloGozalezT <54081160+juanpablogt@users.noreply.github.com>
2023-06-20 06:45:07 +0000

    entendido Estructura

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -14,16 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from my_site.views import home_view, contact_view, about_view, social_view, projects_view, error_view
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from django.http.response import HttpResponse
+from . import views
+
 
 urlpatterns = [
-    path('', home_view, name='home'),
-    path('contact/', contact_view, name='contact'),
-    path('about/', about_view, name='about'),
-    path('social/', social_view, name='social'),
-    path('projects/', projects_view, name='projects'),
     path('admin/', admin.site.urls),
-    path('error/', error_view, name='error')
+    path('my_app/', include('my_app.urls')),
+    path('', views.home_view, name='home')
+    
 ]
+
+
