938c906da4a4a7f780b8c94b12e5d338b0bb74c3
JPabloGozalezT <54081160+juanpablogt@users.noreply.github.com>
2023-06-24 22:57:16 +0000

    Inicio de conexion contemplates

--- my_site/my_app/urls.py
+++ my_site/my_app/urls.py
@@ -4,8 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<topics>/', views.home_view, name='home'),
-    path('<topics>/contact/', views.contact_view, name='contact'),
-    path('<topics>/about/', views.about_view, name='about'),
-    path('<topics>/social/', views.social_view, name='social'),
-    path('<topics>/projects/', views.projects_view, name='projects')
+
 ]
--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -1,52 +1,6 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse, Http404, HttpResponseNotFound
+from django.http import HttpResponse
 
 
-
-articles = {
-    "home": " 666 Juan Pablo",
-    "contact": "Contact Page",
-    "about": "About Page",
-    "social": "Social Page",
-    "projects": "Projects Page",
-    "error": "Error Page" 
-}
-
-# Create your views here.
-def home_view(request,topics):
-    try:
-        result = articles[topics]
-        return HttpResponse(articles[topics])
-    except:
-       raise Http404("no existe la pagina")
-
-def contact_view(request,topics):
-    try:
-        result = articles[topics]
-        return HttpResponse(articles[topics])
-    except:
-       raise Http404("no existe la pagina")
-    
-def about_view(request,topics):
-    try:
-        result = articles[topics]
-        return HttpResponse(articles[topics])
-    except:
-       raise Http404("no existe la pagina")
-
-def social_view(request,topics):
-
-    try:
-        result = articles[topics]
-        return HttpResponse(articles[topics])
-    except:
-       raise Http404("no existe la pagina")
-    
-def projects_view(request,topics):
-
-    try:
-        result = articles[topics]
-        return HttpResponse(articles[topics])
-    except:
-       raise Http404("no existe la pagina")
-
+def home_view(request, topic):
+    return render(request, "my_app/Templates/maqueta1.html" , {"topic": topic})
