efae34ea12993c3e2420be3202aa4b04b6dd9182
JPabloGozalezT <54081160+juanpablogt@users.noreply.github.com>
2023-06-24 23:40:27 +0000

    configurar Templates

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -1,33 +1,18 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
-articles = {
-    "home": " 666 Juan Pablo",
-    "contact": "Contact Page",
-    "about": "About Page",
-    "social": "Social Page",
-    "projects": "Projects Page",
-    "error": "Error Page"
-
-}
-
-# Create your views here.
 def home_view(request):
-    return HttpResponse(articles["home"])
+    return render(request, 'my_app/maqueta1.html ')
 
 def contact_view(request):
-
-    return HttpResponse(articles["contact"])
+    return render(request, 'my_app/maqueta1.html ')
 
 def about_view(request):
-
-    return HttpResponse(articles["about"])
-
-def social_view(request):
-
-    return HttpResponse(articles["social"])
+    return render(request, 'my_app/maqueta1.html ')
 
 def projects_view(request):
+    return render(request, 'my_app/maqueta1.html ')
 
-    return HttpResponse(articles["projects"])
+def social_view(request):
+    return render(request, 'my_app/maqueta1.html ')
 
