9f8f4a73cc948a6d751430087d2cb85b49f00e06
JPabloGozalezT <54081160+juanpablogt@users.noreply.github.com>
2023-06-27 04:00:19 +0000

    llamar diccionario desde template

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def simple_view(request):
-    return render(request, 'first_app/maqueta.html')
+    return render(request,'first_app/maqueta.html')
+
+def contact_view(request):
+    
+    my_var = {'first_name':'Pablo','last_name':'Gonzalez', 'age':'32'}
+    return render(request,'first_app/contact.html',context= my_var)
+
