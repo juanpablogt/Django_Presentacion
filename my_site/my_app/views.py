49df97deeabea66fe62900106c8aace59ea1aaf2
JPabloGozalezT <54081160+juanpablogt@users.noreply.github.com>
2023-06-27 19:31:39 +0000

    for en el template

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -6,6 +6,6 @@ def simple_view(request):
 
 def contact_view(request):
     
-    my_var = {'first_name':'pablo','last_name':'gonzalez', 'age':'32'}
+    my_var = {'first_name':'pablo','last_name':'gonzalez', 'age':'32', 'list':[1,2,3,4,5]}
     return render(request,'first_app/contact.html',context= my_var)
 
