98cc39a3f0cc0dc559a9ce2de47de7def44f4eda
JPablo <juapabgonzalez@utp.edu.co>
2023-07-13 10:52:08 -0500

    Ingresando datos

--- my_site1/office/models.py
+++ my_site1/office/models.py
@@ -6,4 +6,7 @@ class patient (models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} have {self.age} years old."
     
