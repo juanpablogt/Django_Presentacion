3545f9507e6d836c1843201a557c4b7c66422a56
JPablo <juapabgonzalez@utp.edu.co>
2023-07-17 17:00:45 -0500

    CRUD

--- my_card/card/models.py
+++ my_card/card/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Card(models.Model):
+    # Campos del modelo
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
 
-# Create your models here.
+    def __str__(self):
+        return f"datos de la persona: {self.name}  {self.email} {self.phone}, {self.address}"
--- my_card/card/views.py
+++ my_card/card/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from . import models
+from .models import Card
 
 def list(request):
-    return render(request, 'card/list.html')
+    all_cards = models.Card.objects.all()
+    context = {'all_cards': all_cards}
+    return render(request, 'card/list.html', context=context)
 
 def add(request):
-    return render(request, 'card/add.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        address = request.POST['address']
+        
+        new_card = Card(name=name, email=email, phone=phone, address=address)
+        new_card.save()
+        
+        return redirect('card:list')
+    else:
+        return render(request, 'card/add.html')
+
 
 def delete(request):
-    return render(request, 'card/delete.html')
+    if request.method == 'POST':
+        pk=request.POST['pk']
+        try:
+           models.Card.objects.get(pk=pk).delete()
+           return render(request, 'card/delete.html')
+        except:
+            print("Error")
+            return redirect('card:list')
+    else:
+        return render(request, 'card/delete.html')
+    
