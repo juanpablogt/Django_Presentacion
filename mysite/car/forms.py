32e431c32ec2081ebed6d532475eb6779597ace9
JPablo <juapabgonzalez@utp.edu.co>
2023-07-25 18:51:21 -0500

    validacion de formularios

--- mysite/car/forms.py
+++ mysite/car/forms.py
@@ -4,6 +4,17 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['first_name', 'last_name', 'starts']
+        fields = "__all__"
+        labels = {
+            'first_name': 'Your first name',
+            'last_name': 'Your last name',
+            'starts': 'Your rating',
+        }
+        error_messages = {
+            'starts': {
+                'max_value': "Your max value es 5",
+                'min_value': "Your min value es 1",
+            }
+        }
 
   
