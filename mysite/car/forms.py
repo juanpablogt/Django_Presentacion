a71af2c9a8036a1f65c6ed8c923adecbde073de5
JPablo <juapabgonzalez@utp.edu.co>
2023-07-25 17:35:08 -0500

    Conectando con la BD

--- mysite/car/forms.py
+++ mysite/car/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from .models import Review
 
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='Your first name', max_length=100)
-    last_name = forms.CharField(label='Your last name', max_length=100)
-    email = forms.EmailField(label='Your email', max_length=100)
-    review = forms.CharField(label = 'please write your review', widget=forms.Textarea(attrs={'class':'myform'}))#desde aqui se puede controlar la linea punteada que aparece en el campo de mensaje
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['first_name', 'last_name', 'starts']
 
+  
